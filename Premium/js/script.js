ceb5811eec669fadac20a21b9bb7a5f6c6145795
Anastasiya Fedus <fedus.anastasiya@gmail.com>
2020-01-16 20:45:02 +0200

    Initial commit

---
+++ Premium/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.main-slider').slick({
+      // setting-name: setting-value
+        dots: true
+    });
+
+    $('.answer').click(function(){
+        $('.list-chat').fadeToggle(500);
+      });
+
+
+  });
+
+
